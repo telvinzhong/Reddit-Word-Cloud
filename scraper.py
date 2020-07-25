b7f2641e74660b18c8c3689fb1df24c8b96f2038
Telvin <telvinzhong@gmail.com>
2020-07-24 23:32:32 -0700

    Working word cloud and csv generation, word cloud only reads first word in csv

--- scraper.py
+++ scraper.py
@@ -1,39 +1,80 @@
 import praw
+import numpy as np
+import pandas as pd
+from os import path
+from PIL import Image
+import csv
+import nltk
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from nltk.corpus import stopwords
 from praw.models import MoreComments
 
 reddit = praw.Reddit(client_id='qeEY-gzqmsBETQ', client_secret='5kzPlra-qhr2MRt5aYBKWHxRZFY', user_agent='Telvin Zhong')
 
-submission = reddit.submission(url="https://www.reddit.com/r/AskReddit/comments/httuy9/what_is_a_lot_unhealthier_than_most_people_think/")
+submission = reddit.submission(url="https://www.reddit.com/r/worldnews/comments/hwugz3/mike_pompeo_says_free_world_must_change_china_or/")
 submission.comments.replace_more(limit = 0)
 
-dic = {}
-banned = ["\n", ".", "'", ")", "(", "*", "?", "!", "’"]
-common = ["and", "the", "that", "you", "but"]
+# NLTK stopwords corpus contains common English words that we remove from our dataset.
+common = stopwords.words('english')
+print(common)
+all_words = []
+word_count = 0
+banned = ["\n", ".", "'", ")", "(", "*", "?", "!", "’", "[", "]"]
 
 def clean(word):
-
+    # Helper function to ensure alphanumeric, lowercase inputs. 
     res = []
     for i in word:
+        # Remove items like \n and punctuation from the dataset.
         if i in banned:
             continue
         elif i.isalnum:
             res.append(i.lower())
     return "".join(res)
 
-# if isinstance(top_level_comment, MoreComments):      Alternative way to skip more comments
-for comment in submission.comments.list():
-    com = str(comment.body).split(" ")
-    for word in com:
-        word = clean(word)
-        if len(word) >= 4 and word not in common:
-            dic[word] = dic.get(word, 0) + 1
+def main():
+    for comment in submission.comments.list():
+        # Split comments by spaces into words and clean/append words.
+        com = str(comment.body).split(" ")
+        for word in com:
+            word = clean(word)
+            if len(word) >= 3 and word not in common:
+                all_words.append(word)
 
-# print(dic)
-trimming = []
-for key, val in dic.items():
-    trimming.append([key, val])
-trimming.sort(key = lambda x: x[1], reverse=True)
-print(trimming)
-# for i in range(len(trimming) // 4)):
-#     dic.delitem(trimming[i])
-    
+    # Add cleaned words to text.csv.
+    # with open('text.csv', 'w') as myfile:
+    #     wr = csv.writer(myfile, quoting = csv.QUOTE_ALL)
+    #     wr.writerow(all_words)
+
+
+
+    wordcloud = WordCloud().generate(all_words)
+
+    # Display the generated image:
+    # the matplotlib way:
+    import matplotlib.pyplot as plt
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+
+    # lower max_font_size
+    wordcloud = WordCloud(max_font_size=40).generate(all_words)
+    plt.figure()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+
+main()
+
+#--------------------------------------------- ALTERNATIVE ---------------------------------------------------
+
+
+# if isinstance(top_level_comment, MoreComments):      Another way to skip more comments
+
+# dic = {}
+# for comment in submission.comments.list():
+#     com = str(comment.body).split(" ")
+#     for word in com:
+#         word = clean(word)
+#         if len(word) >= 4 and word not in common:
+#             dic[word] = dic.get(word, 0) + 1
---
+++ test.py
@@ -0,0 +1,36 @@
+import praw
+import numpy as np
+import pandas as pd
+from os import path
+from PIL import Image
+import csv
+import nltk
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from nltk.corpus import stopwords
+from praw.models import MoreComments
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(r"text.csv", encoding ="latin-1")
+# Iterating through the .csv data file 
+for i in data: 
+    comment_words = ""
+    i = str(i) 
+    separate = i.split()
+    for j in range(len(separate)): 
+        separate[j] = separate[j].lower() 
+      
+    comment_words += " ".join(separate)+" "
+
+# Creating the Word Cloud
+final_wordcloud = WordCloud(width = 800, height = 800, 
+                background_color ='white', 
+                # stopwords = stop_words, 
+                min_font_size = 10).generate(comment_words)
+
+# Displaying the WordCloud                    
+plt.figure(figsize = (10, 10), facecolor = None) 
+plt.imshow(final_wordcloud) 
+plt.axis("off") 
+plt.tight_layout(pad = 0) 
+  
+plt.show()
